0c427dfc94b79678e9c90301f5fcdbf3811d3332
johnsoap1 <johnsoap1@proton.me>
2026-02-15 18:13:12 +0000

    ii

--- Config.py
+++ Config.py
@@ -1,12 +1,12 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("API_ID"))
-    API_HASH = os.getenv("API_HASH")
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    API_ID = int(os.getenv("API_ID") or "0")
+    API_HASH = os.getenv("API_HASH") or ""
+    BOT_TOKEN = os.getenv("BOT_TOKEN") or ""
 
     STORAGE_GROUP_ID = int(os.getenv("STORAGE_GROUP_ID", "0")) or None
-    OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+    OWNER_ID = int(os.getenv("OWNER_ID", "0")) or None
 
     MAX_ALBUM_SIZE = 10
 
